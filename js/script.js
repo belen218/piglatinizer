8c334558830f6f68fa497c680deda2b70fcceedf
belen218 <belen.szz218@gmail.com>
2019-10-16 01:28:57 +0000

    add click function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 $( "document" ).ready(function() {
 
 
+$('#button').click(function(){
+    console.log('hi')
+    var inputValue = $('.input').val()
+    console.log(inputValue)
+
+    var firstLetter=inputValue[0];
+    var vowels=['a','e','o','u'];
+
+    if (vowels.includes(firstLetter)){
+        return 'starts with vowels'
+    }
+
+    $('.output').text(inputValue)
 
 });
+
+
+
+})
+
+
